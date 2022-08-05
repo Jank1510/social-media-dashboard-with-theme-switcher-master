e97e93a0d176a1876bb6343645e61cc7363d10d2
Jank1510 <89560208+Jank1510@users.noreply.github.com>
2022-08-04 21:46:13 -0500

    update finish

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
+var theme = "dark"
+
 function btn() {
-    console.log("jjasjas")
-    var on = document.getElementById("btn-theme");
+    if (theme == 'dark') {
+
+        var botom = document.getElementsByClassName("btn-theme-off")[0];
+        botom.className = "btn-theme-on";
+
+        var body = document.getElementsByClassName("dark-theme-body")[0];
+        body.className = "white-theme-body ";
 
 
-    var body = document.getElementsByClassName("dark-theme-body")[0];
-    body.className = "white-theme-body ";
-    var cardBbackground = document.getElementsByClassName("dark-mode-card")[0];
-    cardBbackground.className = "white-mode-card ";
+
+        for (let index = 0; index < 12; index++) {
+            var cardBbackground = document.getElementsByClassName("dark-mode-card")[0];
+            cardBbackground.className = "white-mode-card";
+        }
+        theme = 'white'
+    } else {
+        if (theme == 'white') {
+
+
+            var botom = document.getElementsByClassName("btn-theme-on")[0];
+            botom.className = "btn-theme-off";
+            var body = document.getElementsByClassName("white-theme-body")[0];
+            body.className = "dark-theme-body ";
+
+
+
+            for (let index = 0; index < 12; index++) {
+                var cardBbackground = document.getElementsByClassName("white-mode-card")[0];
+                cardBbackground.className = "dark-mode-card";
+            }
+            theme = 'dark'
+        }
+    }
 
 }
